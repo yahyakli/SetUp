ac17e7e87d670630de0a9a83012ed271b59f331c
yahya akli <akli.yahya05@gmail.com>
2025-04-12 18:55:23 +0100

    init billing service

---
+++ services/BillingService/src/test/java/SetUp/Billing/Service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SetUp.Billing.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
